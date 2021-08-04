d40c9b5cb23798b07cf5560476e25561fa1f1b52
Ikram-Maulana <ikram_maulana@onedrive.web.id>
2021-08-04 18:50:53 +0700

    Animations

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -7,27 +7,36 @@ public class playerController : MonoBehaviour
     private Rigidbody2D rigidBody;
     public float thrust = 10.0f;
     public LayerMask groundLayerMask;
+    public Animator animator;
+    public float runSpeed = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator.SetBool("isAlive", true);
+    }
+
+    private void FixedUpdate()
+    {
+        if(rigidBody.velocity.x < runSpeed)
+        {
+            // rigidBody.velocity = new Vector2(x,y);
+            rigidBody.velocity = new Vector2(runSpeed, rigidBody.velocity.y);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         bool isOnTheGround = IsOnTheGround();
-        if (Input.GetMouseButtonDown(0)
+        animator.SetBool("isGrounded", isOnTheGround);
+        if ((Input.GetMouseButtonDown(0)
             || Input.GetKeyDown(KeyCode.Space)
             || Input.GetKeyDown(KeyCode.W)
-            
-            )
+            ) && isOnTheGround)
         {
-            if (isOnTheGround)
-            {
-                Jump();
-            }
+            Jump();
         }
     }
 
@@ -39,6 +48,7 @@ public class playerController : MonoBehaviour
 
     bool IsOnTheGround()
     {
+        // return Physic2D.Raycast(yang ditransform adalah position, vector2.u/d, jarak ground dan bunny, masukan layerMask.value);
         return Physics2D.Raycast(transform.position, Vector2.down, 1.0f, groundLayerMask.value);
     }
 }

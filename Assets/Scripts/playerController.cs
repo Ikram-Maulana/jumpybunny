b95ba249c0fd0313589883495f5270f8539f0637
Ikram-Maulana <ikram_maulana@onedrive.web.id>
2021-08-04 13:12:40 +0700

    Physics2d.Raycast()
    raycast consept. jump when in the ground and fix bug bunny always maju

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -6,6 +6,7 @@ public class playerController : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
     public float thrust = 10.0f;
+    public LayerMask groundLayerMask;
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +17,17 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bool isOnTheGround = IsOnTheGround();
         if (Input.GetMouseButtonDown(0)
             || Input.GetKeyDown(KeyCode.Space)
             || Input.GetKeyDown(KeyCode.W)
             
             )
         {
-            Jump();
+            if (isOnTheGround)
+            {
+                Jump();
+            }
         }
     }
 
@@ -30,5 +35,10 @@ public class playerController : MonoBehaviour
     {
         // Alternatively, specify the force mode, which is ForceMode2D.Force by default
         rigidBody.AddForce(Vector2.up * thrust, ForceMode2D.Impulse);
+    }
+
+    bool IsOnTheGround()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, 1.0f, groundLayerMask.value);
     }
 }
